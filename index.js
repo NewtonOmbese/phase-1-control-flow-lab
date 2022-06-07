db7aeb84b4104e54e2245c795d72066116e9fc9f
NewtonOmbese <newtonombese1@gmail.com>
2022-06-07 09:04:39 +0300

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function scuberGreetingForFeet(feet){
   if (feet <= 400){
     return 'This one is on me!';
   }
+  else if (feet > 400 && feet < 2000){
+    return 'That will be twenty bucks.'
+  }
   else if (1999 < feet && feet < 2500){
     return 'I will gladly take your thirty bucks.'
   }
